121113e4bb4368b71f8b07469d258052602e4d51
Akash Agrawal <akashagrawal@Akashs-MacBook-Pro.local>
2020-07-19 00:37:20 +0530

    test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='akash_example',
+    packages=['akash_example'],
+    description='Hello world enterprise edition',
+    version='0.1',
+    url='http://github.com/example/akash_example',
+    author='Linode',
+    author_email='docs@linode.com',
+    keywords=['pip','linode','example']
+    )
