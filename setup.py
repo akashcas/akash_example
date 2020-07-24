0255324987fcbf7cbfa04049dabfdb7a308705b6
Akash Agrawal <akashagrawal@Akashs-MacBook-Pro.local>
2020-07-25 03:12:18 +0530

    test

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='akash-example',
-    packages=['akash-example'],
-    description='Hello world enterprise edition',
-    version='0.1',
-    url='http://github.com/example/akash-example',
-    author='Linode',
-    author_email='docs@linode.com',
-    keywords=['pip','linode','example']
-    )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="akash-example",
+    version="0.1",
+    author="akash",
+    author_email="akashmagrawal@gmail.com",
+    description="A function that returns 'hello world'",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akashcas/akash_example",
+    packages=setuptools.find_packages()
+)
